3ba66aebc353fab61a42e0902b003c0ba539dffc
jaisuriya978 <jaisuriya978@gmail.com>
2026-02-11 11:42:09 +0530

    msg

---
+++ interview.js
@@ -0,0 +1,81 @@
+    function toggleDarkMode() {
+        document.body.classList.toggle('dark-mode');
+    }
+
+    function toggleFontSize() {
+        document.body.classList.toggle('large-font');
+    }   
+
+    function toggleHighContrast() {
+        document.body.classList.toggle('high-contrast');
+    }
+    function toggleLineSpacing() {
+        document.body.classList.toggle('line-spacing');
+    }
+    function toggleFontFamily() {
+        document.body.classList.toggle('font-family');
+    }
+    function toggleTextAlignment() {
+        document.body.classList.toggle('text-alignment');
+    }   
+    function toggleImageVisibility() {
+        document.body.classList.toggle('hide-images');
+    }
+    function toggleAnimations() {
+        document.body.classList.toggle('disable-animations');
+    }   
+    function toggleKeyboardNavigation() {
+        document.body.classList.toggle('keyboard-navigation');
+    }       
+    function toggleScreenReader() {
+        document.body.classList.toggle('screen-reader');
+    }   
+    function toggleColorBlindMode() {
+        document.body.classList.toggle('color-blind-mode');
+    }
+    function toggleFocusMode() {
+        document.body.classList.toggle('focus-mode');
+    }   
+    function toggleCustomCursor() {
+        document.body.classList.toggle('custom-cursor');
+    }
+    function toggleDarkMode() {
+        document.body.classList.toggle('dark-mode');
+    }
+    function toggleFontSize() { 
+        document.body.classList.toggle('large-font');
+    }   
+    function toggleHighContrast() {
+        document.body.classList.toggle('high-contrast');
+    }
+    function toggleLineSpacing() {
+        document.body.classList.toggle('line-spacing');
+    }
+    function toggleFontFamily() {
+        document.body.classList.toggle('font-family');
+    }
+    function toggleTextAlignment() {
+        document.body.classList.toggle('text-alignment');
+    }
+    function toggleImageVisibility() {
+        document.body.classList.toggle('hide-images');
+    }       
+    function toggleAnimations() {
+        document.body.classList.toggle('disable-animations');
+    }
+    function toggleKeyboardNavigation() {
+        document.body.classList.toggle('keyboard-navigation');
+    }
+
+    function toggleScreenReader() {
+        document.body.classList.toggle('screen-reader');
+    }   
+    function toggleColorBlindMode() {
+        document.body.classList.toggle('color-blind-mode');
+    }   
+    function toggleFocusMode() {
+        document.body.classList.toggle('focus-mode');
+    }
+    function toggleCustomCursor() {
+        document.body.classList.toggle('custom-cursor');
+    }   
